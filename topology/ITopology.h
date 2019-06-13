718cf5aa6dd0fb2e8f00a1077d581cb530ed013f
writesky <38555019+writesky@users.noreply.github.com>
2019-06-13 13:51:28 +0800

    Add files via upload

---
+++ topology/ITopology.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include"F:\whale_storm\base\Values.h"
+
+#include<string>
+#include<memory>
+#include<map>
+
+namespace whale_storm
+{
+	namespace spout
+	{
+		class ISpout;
+	}
+	namespace bolt
+	{
+		class IBolt;
+	}
+
+	namespace topology
+	{
+		class ITopology
+		{
+			~ITopology() {}
+
+			virtual void SetSpouts(std::map<std::string, std::shared_ptr<spout::ISpout>> spouts) = 0;
+			virtual void SetBolts(std::map<std::string, std::shared_ptr<bolt::IBolt>> bolts) = 0;
+			virtual void SetNetwork(std::map<std::string, std::vector<std::string>> network) = 0;
+			virtual void EmitFrom(const std::string& src, const base::Values& values) = 0;
+
+			virtual const std::map<std::string, std::shared_ptr<spout::ISpout>>& GetSpouts() const = 0;
+			virtual const std::map<std::string, std::shared_ptr<bolt::IBolt>>& GetBolts() const = 0;
+			virtual const std::map<std::string, std::vector<std::string>>& GetNetwork() const = 0;
+
+			virtual void Start() = 0;
+		};
+	}
+}
